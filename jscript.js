50f01915aa9880091c8cf23c7c870180b83457ff
Shubham2301 <shubhamjoshi2301@gmail.com>
2017-04-13 16:25:09 +0530

    Update jscript.js
    formatted

--- jscript.js
+++ jscript.js
@@ -1,12 +1,12 @@
 // jquery to apply ajax for form refreshing 
                    
                                
-                               console.log("inside script");
-                              $(document).ready(function(){
+                            console.log("inside script");
+                                 $(document).ready(function(){
                                  $("#save").click(function(){
-                              var name = $("#name").val();
-                              var phone = $("#phone").val();
-                              var email = $("#email").val();
+                                 var name = $("#name").val();
+                                 var phone = $("#phone").val();
+                                 var email = $("#email").val();
                               
                               
                               // Returns successful data submission message when the entered information is stored in database.
@@ -14,23 +14,24 @@
                               console.log(dataString);
 
                               if(name==''||phone==''||email=='')
-                              {
-                              alert("Please Fill All Fields");
-                              }
+                                {
+                                 alert("Please Fill All Fields");
+                                }
+                                   
                               else
                               {
-                              // AJAX Code To Submit Form.
-                              $.ajax({
-                              type: "POST",
-                              url: "f.php",
-                              data: dataString,
-                              success: function(result){
-                              console.log("ajax request complete");
+                               // AJAX Code To Submit Form.
+                               $.ajax({
+                               type: "POST",
+                               url: "f.php",
+                               data: dataString,
+                               success: function(result){
+                               console.log("ajax request complete");
                              
                               //alert(result);
-                              }
-                              });
-                              }
-                              });
-                              });
+                              }//end success
+                              });//end ajax
+                              }//end else
+                              });//end click function
+                              });//end ready function
              
